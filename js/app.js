132b6f2442ae5b33a63abbecf74d9e64202edf8f
Huw Fernie <HuwFernie@gmail.com>
2017-12-04 12:43:08 +0000

    added left and right movement to me

--- js/app.js
+++ js/app.js
@@ -2,15 +2,43 @@ console.log('Hello Huw');
 
 
 $(() => {
+
+  const $me = document.getElementById('me');
+
+
   document.addEventListener('keydown', (event) =>{
     const keyName = event.key;
     if (keyName === 'ArrowLeft') {
       console.log('left');
+      return moveLeft();
     } else if (keyName === 'ArrowRight') {
       console.log('right');
+      return moveRight();
     } else {
       console.log('other Key');
     }
   });
+
+
+  const me = {};
+  me.xPos = 15;
+  me.yPos = 0;
+
+
+  function moveRight(){
+    console.log('moveRight');
+    me.xPos++;
+    updatePosn();
+  }
+
+  function moveLeft(){
+    console.log('moveRight');
+    me.xPos--;
+    updatePosn();
+  }
+
+  function updatePosn(){
+    $me.style.left = `${me.xPos}px`;
+  }
 
 });
